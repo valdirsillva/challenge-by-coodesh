90fe9b537641d52e3cf58743b5928549b530b3ce
Valdir Silva <valdipiresba@gmail.com>
2023-08-01 22:49:58 -0300

    Adiciona rota para retornar detalhes uso de memoria e conectividade da api

--- src/interfaces/routes/routes.ts
+++ src/interfaces/routes/routes.ts
@@ -1,16 +1,57 @@
-import { Router } from 'express'
+import os from 'os'
+
+import { Router, response } from 'express'
 import { createProductController } from '../../use-cases/CreateProduct';
-import { getProductController } from '../../use-cases/GetProduct';
+import { getProductByCodeController, getProductController } from '../../use-cases/GetProduct';
 import { deleteProductController } from '../../use-cases/DeleteProduct';
+import { Database } from '../../infrastructure/database/mongodb/mongoose.service';
 
 const router = Router()
 
-router.post('/api/add', (request, response) => {
+function managerMemoryRam() {
+    const usedMemory = process.memoryUsage();
+
+    console.log('Memory usage (RSS):', formatBytes(usedMemory.rss));
+    console.log('Heap total:', formatBytes(usedMemory.heapTotal));
+    console.log('Heap used:', formatBytes(usedMemory.heapUsed));
+
+    function formatBytes(bytes: any) {
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+        if (bytes === 0) return '0 Byte';
+        const i = Math.floor(Math.log(bytes) / Math.log(1024))
+        return Math.round((bytes / Math.pow(1024, i)) * 100) / 100 + ' ' + sizes[i];
+    }
+}
+
+router.get("/", async (request, response) => {
+    const status = Database.getConnectionStatus();
+
+    console.log('-----------------------------')
+    console.log('MONGODB STATUS:', status);
+    console.log('-----------------------------')
+
+    setInterval(() => {
+        console.clear()
+        managerMemoryRam()
+    }, 2000)
+
+
+    return response.status(200).json({
+        status: status
+    })
+
+})
+
+router.put('/api/add', (request, response) => {
     return createProductController.handle(request, response)
 })
 
 router.get('/api/products', (request, response) => {
     return getProductController.handle(request, response);
+})
+
+router.get('/api/products/:code', (request, response) => {
+    return getProductByCodeController.handle(request, response)
 })
 
 router.delete('/api/products/:code', (request, response) => {
